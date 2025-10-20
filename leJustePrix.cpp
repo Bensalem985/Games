5183807f1cf67442706d3413a30b5a6ed530e53c
Bensalem985 <mekontsobenimaru@gmail.com>
2025-10-20 09:59:10 +0100

    Add files via upload

---
+++ leJustePrix.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main() {
+    // Initialisation du générateur aléatoire
+    srand(time(nullptr));
+    
+    // Génération du nombre secret entre 1 et 100
+    int nombreSecret = rand() % 100 + 1;
+    int tentative = 0;
+    int proposition = 0;
+    
+    std::cout << "=== LE JUSTE PRIX ===" << std::endl;
+    std::cout << "J'ai choisi un nombre entre 1 et 100. Devinez-le !" << std::endl << std::endl;
+    
+    // TODO: Ajouter la boucle de jeu ici
+    do {
+
+        // Calcul du nombre de tentatives
+        tentative++;
+
+        // Lecture de la proposition du joueur
+        std::cout << "Votre proposition: ";
+        std::cin >> proposition;
+
+        // Verification de la proposition du joueur
+        if (proposition < nombreSecret) {
+            std::cout << "-> Plus grand!!" << std::endl << std::endl;
+        } else if(proposition > nombreSecret) {
+            std::cout << "-> Plus petit!!" << std::endl << std::endl;
+        } else {
+            std::cout << "Bravo!!!  vous avez trouve en " << tentative << " tentatives!" << std::endl;
+        }
+    } while (proposition != nombreSecret);
+    
+    return 0;
+}
